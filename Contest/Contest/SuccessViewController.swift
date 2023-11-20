83c5c72fbeb55a6c0233ac526ae353730ec058fa
etarchibald <ethanarchibald@Veronica.local>
2023-11-20 07:26:57 -0700

    init commit

---
+++ Contest/Contest/SuccessViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SuccessViewController.swift
+//  Contest
+//
+//  Created by Ethan Archibald on 11/17/23.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    @IBOutlet weak var successLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        successLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 6)
+        successLabel.center.x += 300
+        
+        UIView.animate(withDuration: 2, delay: 0, options: [.repeat]) {
+            self.successLabel.center.x -= 300
+            self.successLabel.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }
+        
+    }
+    
+    
+
+}
---
+++ Contest/Contest/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Contest
+//
+//  Created by Ethan Archibald on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet var prizesLabels: [UILabel]!
+    
+    let colors = [UIColor.red, UIColor.blue, UIColor.magenta, UIColor.orange, UIColor.purple, UIColor.green, UIColor.cyan]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        foreverJumpingAnimation(timedelay: 0, IncreaseBy: 0)
+    }
+    
+    func foreverJumpingAnimation(timedelay: Int, IncreaseBy: Int) {
+        UIView.animate(withDuration: 1.0, delay: Double(timedelay), usingSpringWithDamping: 1.0, initialSpringVelocity: 2.0, options: [.repeat], animations: {
+            
+            let translate1 = CGAffineTransform(translationX: 0, y: -50)
+            
+            for each in self.prizesLabels {
+                each.textColor = self.colors.randomElement()
+                each.transform = translate1
+            }
+            
+        }, completion: nil)
+    }
+    
+    func textFieldAnimation() {
+        UIView.animate(withDuration: 0.1, delay: 0, animations: {
+            
+            let translate1 = CGAffineTransform(translationX: 0, y: -20)
+            
+            self.emailTextField.transform = translate1
+        }) { (_) in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.emailTextField.transform = CGAffineTransform.identity
+            })
+        }
+    }
+
+    @IBAction func submitButton(_ sender: UIButton) {
+        if emailTextField.text == "" {
+            textFieldAnimation()
+            for each in self.prizesLabels {
+                each.textColor = self.colors.randomElement()
+            }
+        } else {
+            performSegue(withIdentifier: "toOther", sender: sender)
+        }
+        
+    }
+    
+}
+
